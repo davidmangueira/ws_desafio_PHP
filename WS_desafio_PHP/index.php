f932b62c8206c0ba9698fafae26588e5add938c5
davidmangueira <78989517+davidmangueira@users.noreply.github.com>
2021-03-05 08:49:40 -0300

    Add files via upload

---
+++ WS_desafio_PHP/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Desafio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Desafio WS Solutions</h1>
+        <h2>Formularios com PHP</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <ul>
+                        <li><a href="formulario.php">Formulario</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        David M Vieira <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
